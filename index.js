2410e5547ef2047f4fb10fd44dd2d179507c136b
MgKyaw <mgkyaw20112@gmail.com>
2025-02-24 23:20:32 +0700

    feat: Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // console.log('Hello, world! 🙌');
-var aged = true;
-var realAge = 0;
-if (aged) {
-    realAge = 4;
-}
-var dogAge = realAge * 7;
-console.log("".concat(dogAge, " years"));
+// let aged = true;
+// let realAge = 0;
+// if (aged) {
+//     realAge = 4;
+// }
+// let dogAge = realAge * 7;
+// console.log(`${dogAge} years`);
+var firstName = 'muriel!';
+console.log(firstName.toUpperCase());
+console.log(firstName.length);
